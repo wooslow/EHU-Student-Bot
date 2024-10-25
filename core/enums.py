600161204f5b2a9b23f3545996df361d2bc09e4c
Earl <84588908+earluv@users.noreply.github.com>
2024-10-25 12:29:04 +0300

    update(enums.py): add new enums

--- core/enums.py
+++ core/enums.py
@@ -1,1 +1,7 @@
-from enum import IntEnum
+from enum import IntEnum, StrEnum
+
+
+class URLs(StrEnum):
+    BASE_URL = "https://moodle.ehu.lt/"
+    LOGIN_PAGE = "login/index.php"
+    COURSE_PAGE = "my/courses.php"
